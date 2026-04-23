f78640a80686144b0d070a1f23746fb6cd199e4e
itsbroskieblox-byte <itsbroskieblox@gmail.com>
2026-04-23 17:29:36 +0800

    Create ToiletHQ.lua

---
+++ macros/ToiletHQ.lua
@@ -0,0 +1,49 @@
+return {
+    Settings = {
+        Elevators = {
+            "Elevator13",
+            "Elevator14",
+            "Elevator15",
+            "Elevator16"
+        }
+    },
+
+    Positions = {
+        ["Mech Cameraman"] = {
+            CFrame.new(-26.57, 88.97, -79.43)
+        },
+        ["Upgraded Titan Cameraman"] = {
+            CFrame.new(-27.40, 90.34, -78.89)
+        },
+        ["Upgraded Speakerwoman"] = {
+            CFrame.new(-28.43, 84.69, -65.10)
+        },
+    },
+
+    Prices = {
+        ["Mech Cameraman"] = {400, 300, 500, 800, 1000},
+        ["Upgraded Speakerwoman"] = {250, 450, 1000, 1500},
+        ["Upgraded Titan Cameraman"] = {1500, 1500, 3000, 4000, 8000, 10000},
+    },
+
+    Steps = {
+        {action = "set", target = "Skip", value = true},
+
+        {action="place", tower="Upgraded Speakerwoman"},
+        {action="upgrade", tower="Upgraded Speakerwoman", level=2},
+        {action="upgrade", tower="Upgraded Speakerwoman", level=3},
+        {action="upgrade", tower="Upgraded Speakerwoman", level=4},
+
+        {action="fullPlace", tower="Mech Cameraman"},
+      
+        {action = "set", target = "Skip", value = true, condition = {type = "wave", value = 25}},
+      
+        {action="fullPlace", tower="Upgraded Titan Cameraman"},
+
+        {action="fullPlace", tower="Mech Cameraman", count=8},
+      
+        {action="sell", tower="Upgraded Speakerwoman", level=3},
+
+        {action="fullPlace", tower="Upgraded Titan Cameraman", count=5},
+    }
+}
